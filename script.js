098bcae70f173abed57b5645c5800a76dc99e713
Berresu <90646787+Berresu@users.noreply.github.com>
2022-09-18 21:15:42 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+let cumle=document.getElementById("tersCevir");
+let tamam=document.getElementById("btnTamam");
+let sonuc=document.getElementById("sonuc");
+
+tamam.addEventListener("click", ()=>{
+    sonuc.innerHTML=cumle;
+})
