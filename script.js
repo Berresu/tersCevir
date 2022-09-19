5c2bf316e225de0a0e4d26899dd59c27faea1530
Berresu <90646787+Berresu@users.noreply.github.com>
2022-09-19 08:56:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,2 @@
-let cumle=document.getElementById("tersCevir");
-let tamam=document.getElementById("btnTamam");
-let sonuc=document.getElementById("sonuc");
-
-tamam.addEventListener("click", ()=>{
-    sonuc.innerHTML=cumle;
-})
+let sayilar=["0","1","2","3","4","5","6","7","8","9"];
+document.getElementById("sonuc").innerHTML=sayilar.reverse();
